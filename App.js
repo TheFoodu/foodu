f5302ed900fdeb20890101a5d7279637aabb9e76
Nick Teets <nickteets@gmail.com>
2018-04-23 19:09:02 -0600

    Initial commit - add Foodu logo and install prettier

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image 
+          style={{height: 50, width: 103}}
+          source={require('./assets/foodu_logo.png')}
+        />
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+  },
+});
