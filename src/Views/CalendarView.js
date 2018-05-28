587f8d041ac24b1a4f607aea415329fc982bf462
Dione Wilson <dionew1@gmail.com>
2018-05-28 16:51:51 -0600

    WIP markedDates

--- src/Views/CalendarView.js
+++ src/Views/CalendarView.js
@@ -1,12 +1,21 @@
 import React from "react";
 import moment from "moment";
 import { StyleSheet, Text, Dimensions, View } from "react-native";
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars';
 const { width } = Dimensions.get('window');
 
 export default class CalendarView extends React.Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            markedDates: []
+        }
+    }
     static navigationOptions = {
         title: "Calendar",
+    };
+    markDate(day) {
+        this.state.markedDates.push(day.dateString) 
     };
     render() {
         const { navigate } = this.props.navigation;
@@ -17,6 +26,8 @@ export default class CalendarView extends React.Component {
                         textDayFontFamily: 'montserrat',
                         textMonthFontFamily: 'montserrat',
                         textDayHeaderFontFamily: 'montserrat',
+                    }}
+                    onDayPress={(day) => { this.markDate(day)
                     }}></Calendar>
             </View>
         );
