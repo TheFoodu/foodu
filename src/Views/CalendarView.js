be717fb2d7d6c0e4a2b080018ccfa4023b62ab7d
Dione Wilson <dionew1@gmail.com>
2018-05-28 17:37:21 -0600

    Add toggle to select dates on calendar view.

--- src/Views/CalendarView.js
+++ src/Views/CalendarView.js
@@ -7,32 +7,45 @@ const { width } = Dimensions.get('window');
 export default class CalendarView extends React.Component {
     constructor(props){
         super(props)
-        this.state= {
-            markedDates: []
-        }
+        this.state = { markedDates: { "2018-05-16": { selected: true, marked: true, selectedColor: "#E06C63" } } };
     }
     static navigationOptions = {
         title: "Calendar",
     };
-    markDate(day) {
-        this.state.markedDates.push(day.dateString) 
+
+    markDate = (day) => {
+        const selectedDay = moment(day.dateString).format("YYYY-MM-DD");
+        let selected = true;
+        if (this.state.markedDates[selectedDay]) {
+            // Already in marked dates, so reverse current marked state
+            selected = !this.state.markedDates[selectedDay].selected;
+        }
+
+        // Create a new object using object property spread since it should be immutable
+        // Reading: https://davidwalsh.name/merge-objects
+        const updatedMarkedDates = { ...this.state.markedDates, ...{ [selectedDay]: { selected, selectedColor: "#E06C63" } } };
+
+        // Triggers component to render again, picking up the new state
+        this.setState({ markedDates: updatedMarkedDates });
     };
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.container}>
                 <Calendar 
+                    markedDates={this.state.markedDates}
                     theme={{
                         textDayFontFamily: 'montserrat',
                         textMonthFontFamily: 'montserrat',
                         textDayHeaderFontFamily: 'montserrat',
                     }}
-                    onDayPress={(day) => { this.markDate(day)
-                    }}></Calendar>
+                    onDayPress={this.markDate}
+                ></Calendar>
             </View>
         );
     }
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
